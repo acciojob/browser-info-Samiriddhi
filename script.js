6b7352fa54393b35822e216e2278857fe405fc4f
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-07-25 01:02:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+let browserName="mozilla";
+let version="10.0";
+document.getElementById("browser-info").innerHTML = `You are using   ${browserName}   version  ${version}`
